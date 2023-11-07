e1ca9c5a1c6378138d7d82eed2ac03cb54417ced
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-11-07 10:52:15 +0100

    daisy ui

--- src/components/FilterSection.tsx
+++ src/components/FilterSection.tsx
@@ -4,20 +4,19 @@ import { tabImages } from "../assets/data";
 
 const FilterSection = () => {
   return (
-    <div className=" fixed md:top-20 md:w-screen top-[78px] z-30 drop-shadow-md bg-white flex items-center overflow-x-scroll scrollbar-hide justify-between pt-8 md:pb-5 px-8">
+    <div className=" fixed md:top-20 md:w-screen top-[78px] z-30 drop-shadow-md md:drop-shadow-none bg-white flex items-center overflow-x-scroll scrollbar-hide justify-between pt-8 md:pb-5 px-8">
       <div className="  relative flex items-center gap-5 ">
         <div className=" bg-gradient-to-r  from-[#ffffff]  z-20 h-16  w-[5rem] absolute -left-[24px] " />
-        <div className=" bg-gradient-to-l  from-[#ffffff]  z-20 h-16  w-[5rem] absolute -right-[24px]  " />
+        <div className=" bg-gradient-to-l  from-[#ffffff]  z-20 h-16  w-[5rem] absolute -right-[24px] md:-right-[5px]  " />
         <BiChevronLeft className="text-3xl hidden md:flex z-20 border rounded-full cursor-pointer text-black" />
 
-        <div className="relative cursor-pointer flex gap-16 w-[80%] md:w-auto md:gap-12   justify-center overflow-x-clip scrollbar-hide items-center">
+        <div className="relative cursor-pointer flex gap-16 w-[80%] md:w-[90%] md:gap-12   justify-center overflow-x-clip scrollbar-hide items-center">
           {tabImages?.map((i, index) => (
-            <div className="flex flex-col items-center cursor-pointer justify-center">
-              <img
-                className="h-6 text-[#717171] grayscale"
-                src={i.img}
-                key={index}
-              />
+            <div
+              key={index}
+              className="flex flex-col items-center cursor-pointer justify-center"
+            >
+              <img className="h-6 text-[#717171] grayscale" src={i.img} />
               <p className="text-xs font-semibold">{i.name}</p>
             </div>
           ))}
@@ -29,8 +28,8 @@ const FilterSection = () => {
         <button className=" text-xs border  font-semibold  rounded-xl border-slate-300 p-3 flex items-center justify-center gap-2">
           <LuSettings2 className="text-lg" /> Filters
         </button>
-        <span className=" text-xs p-3 border font-semibold  rounded-xl border-slate-300 ">
-          Display total before taxes
+        <span className=" text-xs p-3 border font-semibold flex items-center  gap-2 rounded-xl border-slate-300 ">
+          Display total before taxes <input type="checkbox" className="toggle"  />
         </span>
       </div>
     </div>
